b95504da67b170295be85b1d585259256c703299
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-10-21 17:34:12 +0530

    misc

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const router = express.Router();
 
-router.route("/abc").get(async (req, res) => {
+router.route("/").get(async (req, res) => {
   try {
     res.status(200).json({ cart: { HELLO: "WORLD" } });
   } catch (error) {
@@ -13,7 +13,7 @@ router.route("/abc").get(async (req, res) => {
   }
 });
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/.netlify/functions/products`, router);
 
 export default app;
 export const handler = serverless(app);
