bd43c934230522b3d7e87b3cfcee01129cc03183
Avinash Prajapati <avinashprajapati914@gmail.com>
2022-10-21 17:25:37 +0530

    misc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import { api } from "./api";
+import api from "./api";
 const app = express();
 app.use(express.json());
-
-app.use("/", api);
 
 app.listen(port, () => {
   console.log("server started on port " + port);
